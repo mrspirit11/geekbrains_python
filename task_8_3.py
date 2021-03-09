4e412790e4e542e726c342e369e6b91135d9af01
mrspirit <mrspirit11@gmail.com>
2021-03-09 10:08:19 +0300

    Update task_8_3.py

--- task_8_3.py
+++ task_8_3.py
@@ -16,4 +16,27 @@
 # >>> a = calc_cube(5)
 # calc_cube(5: <class 'int'>)
 
+def type_logger(func):
 
+    def wrap(*args, **kwargs):
+        func_val = func(*args, **kwargs)
+        args_str = [f'{i}: {type(i)}' for i in args] + [f'{k}={v}: {type(v)}' for k, v in kwargs.items()]
+        print(f'{func.__name__}({", ".join(args_str)})', f'-> {type(func_val)}')
+        return func_val
+    return wrap
+
+
+@type_logger
+def calc_cube(x):
+    return x ** 3
+
+
+@type_logger
+def s_sum(x, y, z):
+    return x + y + z
+
+
+if __name__ == '__main__':
+
+    print(calc_cube(3))
+    print(s_sum(2, 5, z=2.5))
