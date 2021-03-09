7875ec91df08c830f19a6700b9d684974477da9f
mrspirit <mrspirit11@gmail.com>
2021-03-09 10:29:31 +0300

    Update task_8_4.py

--- task_8_4.py
+++ task_8_4.py
@@ -18,3 +18,30 @@
 
 
 # Примечание: сможете ли вы замаскировать работу декоратора?
+
+def val_checker(ver_func):
+    def _val_checker(func):
+        def wrapper(*args, **kwargs):
+            if all((ver_func(i) for i in args)):
+                return func(*args)
+            else:
+                raise ValueError(f'wrong val {args}')
+        return wrapper
+    return _val_checker
+
+
+@val_checker(lambda x: x > 0)
+def calc_cube(x):
+    return x ** 3
+
+
+@val_checker(lambda x: x > 0)
+def test_calc(x, y, z):
+    return x * y * z
+
+
+if __name__ == '__main__':
+    a = calc_cube(3)
+    print(a)
+    b = test_calc(2, 2, -2)
+    print(b)
