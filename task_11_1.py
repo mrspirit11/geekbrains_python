3de4fe0b2c1953327cd27a88a9693ba28587f36c
evgenijkorovin <mrspirit11@mail.com>
2021-03-21 18:31:16 +0300

    edit task_11_1

--- task_11_1.py
+++ task_11_1.py
@@ -2,15 +2,37 @@
 
 from datetime import date
 
+# class Date:
+#     _date: str
+#     @classmethod
+#     def __init__(cls, dmy: str) -> None:
+#         cls._date = dmy
+
+#     @classmethod
+#     def str_to_dmy(cls) -> map:
+#         return map(int, cls._date.split('-'))
+
+#     @staticmethod
+#     def date_validation(d: int, m: int, y: int) -> date:
+#         try:
+#             return date(y, m, d)
+#         except ValueError as e:
+#             print(e)
+
+
+# d1 = Date('1-12-2020')
+# # print(*d1.str_to_dmy())
+# print(Date.date_validation(*d1.str_to_dmy()))
+
+
 class Date:
-    _date: str
-    @classmethod
-    def __init__(cls, dmy: str) -> None:
-        cls._date = dmy
+
+    def __init__(self, dmy: str) -> None:
+        self._date = self.__class__.date_validation(*self.__class__.str_to_dmy(dmy))
 
     @classmethod
-    def str_to_dmy(cls) -> map:
-        return map(int, cls._date.split('-'))
+    def str_to_dmy(cls, obj: str) -> map:
+        return map(int, obj.split('-'))
 
     @staticmethod
     def date_validation(d: int, m: int, y: int) -> date:
@@ -18,8 +40,11 @@ class Date:
             return date(y, m, d)
         except ValueError as e:
             print(e)
+    
+    def __str__(self) -> str:
+        return str(self._date)
 
 
 d1 = Date('1-12-2020')
 # print(*d1.str_to_dmy())
-print(Date.date_validation(*d1.str_to_dmy()))
+print(d1)
